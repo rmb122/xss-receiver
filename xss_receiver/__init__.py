a86e01f3c31fa96eae5ad158bdda36c69edbb335
rmb122 <me@rmb122.cn>
2022-01-11 01:22:56 +0800

    Update: 统一文件命名

--- xss_receiver/__init__.py
+++ xss_receiver/__init__.py
@@ -7,19 +7,6 @@ from xss_receiver.asserts.ip2region import Ip2Region
 app = sanic.Sanic(__name__)
 config = Config()
 
-import xss_receiver.response
-
-@app.route('/')
-def index(request):
-    print(config.RECV_MAIL_ADDR)
-    return sanic.response.text('123')
-
-
-@app.route('/set', methods=["POST"])
-def set(request: sanic.Request):
-    config.RECV_MAIL_ADDR = request.form['asd']
-    return sanic.response.text('123')
-
 '''
 
 ip2Region = Ip2Region(f'{app.root_path}/asserts/ip2region.db')
--- xss_receiver/controllers/__init__.py
+++ xss_receiver/controllers/__init__.py
@@ -1,14 +1,14 @@
 from xss_receiver.config import URL_PREFIX
 
 from xss_receiver import app
-from .AccessLogController import access_log_controller
-from .AuthController import auth_controller
-from .ConfigController import config_controller
-from .IndexController import index_controller
-from .RuleController import rule_controller
-from .SystemlogController import system_log_controller
-from .TempFileController import temp_file_controller
-from .UploadFileController import upload_file_controller
+from .access_log_controller import access_log_controller
+from .auth_controller import auth_controller
+from .config_controller import config_controller
+from .index_controller import index_controller
+from .rule_controller import rule_controller
+from .system_log_controller import system_log_controller
+from .temp_file_controller import temp_file_controller
+from .upload_file_controller import upload_file_controller
 
 app.register_blueprint(config_controller, url_prefix=URL_PREFIX + '/api/config')
 app.register_blueprint(system_log_controller, url_prefix=URL_PREFIX + '/api/system_log')
